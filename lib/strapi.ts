e59d0cf8817c116ff4535f7ff0c1f6bf236af6dd
abhaykantmishra <abhaykantmishra004@gmail.com>
2025-08-28 20:44:48 +0530

    fix: add try catch block

--- lib/strapi.ts
+++ lib/strapi.ts
@@ -1,18 +1,20 @@
 const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
 
 export async function fetchFromStrapi(path: string) {
-    const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
-    const res = await fetch(`${baseUrl}${path}`, { cache: 'no-store' });
-    if (!res.ok) {
-      throw new Error(`Failed to fetch ${path}`);
-    }
+  try {
+     const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
+    const res = await fetch(`https://portfolio-cms-a0hn.onrender.com/api${path}`, { cache: 'no-store' });
     return res.json();
+  } catch (error) {
+    throw error
+  }  
+    
 }
 
 export function getStrapiMedia(url: string) {
-    const baseUrl = process.env.STRAPI_URL || 'http://localhost:1337';
+    const baseUrl = process.env.NEXT_PUBLIC_STRAPI_URL || 'http://localhost:1337';
     if (!url) return '';
     // If already an absolute URL (like from Cloudinary), just return it
     if (url.startsWith('http')) return url;
-    return `${baseUrl}${url}`;
+    return `https://portfolio-cms-a0hn.onrender.com/api${url}`;
 }
