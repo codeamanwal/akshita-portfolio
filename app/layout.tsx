d3eb3ee5b8fbef1e4983cc5d3ec4336f02b77ed4
hsaini-2002 <harshsainikp2002@gmail.com>
2025-09-12 19:54:45 +0530

    Fixed Google Console

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {ThemeProvider} from "../components/theme-provider";
 import "./globals.css";
 import { DM_Sans, Domine } from "next/font/google";
 import HeroBanner from "@/components/landing_page/footer";
+import Navbar from "@/components/navbar";
 // import "./globals.css";
 
 const dmSans = DM_Sans({
@@ -19,7 +20,7 @@ export const metadata: Metadata = {
   description: "Design partner for consumer brands. I craft identities and packaging that connect, sell, and scale.",
   
   openGraph: {
-    title: "agarwal",
+    title: "Akshita",
     description: "That's where design comes in. Not as the final layer, but as a strategic tool - one that turns positioning into presence, and decisions into identity. It's what ...",
     url: "https://www.akshitaagarwal.com/", // <-- change to your real domain
     siteName: "Portfolio",
@@ -36,7 +37,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    title: "agarwal",
+    title: "Akshita",
     description: "That's where design comes in. Not as the final layer, but as a strategic tool - one that turns positioning into presence, and decisions into identity. It's what ...",
     images: ["https://res.cloudinary.com/dqdwmctpi/image/upload/v1757573436/akshita_favicon_qauadf.png"], // <-- same as above
   },
@@ -51,6 +52,39 @@ export default function RootLayout({
     <html lang="en" className={`${dmSans.variable} ${domine.variable}`}>
       <head>
       <meta name="google-site-verification" content="google0cb3aff4c07362a2.html" />
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: `{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "url": "https://www.akshitaagarwal.com",
+  "name": "Akshita Agarwal",
+  "department": [
+    {
+      "@type": "Organization",
+      "name": "Contact",
+      "url": "https://www.akshitaagarwal.com/contact"
+    },
+    {
+      "@type": "Organization",
+      "name": "About",
+      "url": "https://www.akshitaagarwal.com/about"
+    },
+    {
+      "@type": "Organization",
+      "name": "Work",
+      "url": "https://www.akshitaagarwal.com/work"
+    },
+    {
+      "@type": "Organization",
+      "name": "Play",
+      "url": "https://www.akshitaagarwal.com/play"
+    },
+    {
+      "@type": "Organization",
+      "name": "Journals",
+      "url": "https://www.akshitaagarwal.com/journals"
+    }
+  ]
+}` }} />
       {/* <meta name="google-site-verification" content="s-1jfW3Ap-5oKqB-12GxnPYzR6yJVC7tBvlIMrmBU2c" /> */}
       </head>
       <body className="font-dm-sans">
@@ -60,13 +94,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {/* <nav style={{ display: "flex", gap: "1.5rem", padding: "1rem"  }}>
-              <a href="/contact">Contact</a>
-              <a href="/about">About</a>
-              <a href="/work">Work</a>
-              <a href="/play">Shelf</a>
-              <a href="/journals">Journals</a>
-          </nav> */}
           <main className="min-h-screen font-body">{children}</main>
           <HeroBanner />
         </ThemeProvider>
